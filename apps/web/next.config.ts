b5a551914d7809d5569d5e434fce6e9615687714
MohamedMiladi777 <miladyshady@gmail.com>
2026-07-19 18:27:34 +0100

    created responsive splash screen

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@workspace/ui"],
+   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+  },
 }
 
 export default nextConfig
